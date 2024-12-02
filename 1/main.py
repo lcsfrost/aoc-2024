8f1db145efcaf8508c8fc803324579268b7c9da8
lcsfrst <114206355+lcsfrst@users.noreply.github.com>
2024-12-02 10:20:11 -0800

    s
    s

--- 1/main.py
+++ 1/main.py
@@ -9,9 +9,9 @@ total_distance = 0
 list_a = []
 list_b = []
 
-with open("1.csv") as f:
+with open("locations.csv") as f:
     reader = csv.reader(f, delimiter=" ")
-    #Fucked up and pasted with triple spaces lol.
+    #Fucked up and pasted with triple spaces lol, list indexing is to avoid garbled spaces in the middle
     for i in reader:
         list_a.append(int(i[0])) 
         list_b.append(int(i[-1]))
@@ -21,13 +21,16 @@ list_b.sort() #Sorty sort sort
 
 frequency_dict = {}
 for a,b in zip(list_a,list_b):
-    total_distance += abs(a-b)
-    frequency_dict[b] = frequency_dict.get(b,0) +1
-print (total_distance)
+    total_distance += abs(a-b) #abs as we don't care about direction, just magnitude
+    frequency_dict[b] = frequency_dict.get(b,0) +1 #storing the frequency each number occurs in list 2
 
+
+#############################################################################33
+# PART B
+#############################################################################33
 
 similarity_score = 0
 for a in list_a:
      similarity_score += a * frequency_dict.get(a,0)
-print(f"{similarity_score =}")
-print(f"{total_distance =}")
+print(f"{similarity_score = }")
+print(f"{total_distance = }")
